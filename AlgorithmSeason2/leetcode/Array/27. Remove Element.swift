725db92929898fd10e74103e34ed50209fd3cc26
SangJin Han <s202188@naver.com>
2024-05-01 14:25:18 +0900

    Update 27. Remove Element.swift

--- AlgorithmSeason2/leetcode/Array/27. Remove Element.swift
+++ AlgorithmSeason2/leetcode/Array/27. Remove Element.swift
@@ -9,8 +9,26 @@ import Foundation
 
 extension Solution {
     /// [3, 2, 2, 3]
+    @_optimize(speed)
     func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
         nums.removeAll(where: { $0 == val })
         return nums.count
     }
 }
+
+/**
+ final class Solution {
+     @_optimize(speed)
+     func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+         guard !nums.isEmpty else { return 0 }
+         var i = 0
+         nums.withUnsafeBufferPointer { buffer in
+             for j in buffer.indices where buffer[j] != val {  
+                 nums[i] = buffer[j]
+                 i += 1
+             }
+         }
+         return i
+     }
+ }
+ */
